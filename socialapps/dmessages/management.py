22d55a61a30af58eefb97104c1f9887c6e224caa
Erik Rivera <erik.river@gmail.com>
2012-05-25 14:50:22 -0500

    new notifications schema

--- socialapps/dmessages/management.py
+++ socialapps/dmessages/management.py
@@ -5,12 +5,12 @@ from django.utils.translation import ugettext_noop as _
 
 
 if "notification" in settings.INSTALLED_APPS:
-    from notification import models as notification
+    from notification.models import NoticeType
     
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("user_message", _("Message you"), _("Someone send you a message"), default = 2)
+        NoticeType.create("user_message", _("Message you"), _("Someone send you a message"), default = 2)
     
-    signals.post_syncdb.connect(create_notice_types, sender=notification)
+    signals.post_syncdb.connect(create_notice_types, sender=NoticeType)
 
 else:
     print "Skipping creation of NoticeTypes as notification app not found"
