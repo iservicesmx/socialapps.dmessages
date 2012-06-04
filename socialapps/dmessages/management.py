320247a8a3870a4275ea7bc735d0344fc16c9e3d
Erik Rivera <erik.river@gmail.com>
2012-06-04 18:35:57 -0500

    repair notifications

--- socialapps/dmessages/management.py
+++ socialapps/dmessages/management.py
@@ -1,16 +1,10 @@
 from django.db.models import signals
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import ugettext_noop as _
+from notification.models import NoticeType
+from django.contrib.sites import models as site_app
 
-
-if "notification" in settings.INSTALLED_APPS:
-    from notification.models import NoticeType
+def create_notice_types(app, created_models, verbosity, **kwargs):
+    print "create message notifications"
+    NoticeType.create("user_message", _("Message you"), _("Someone send you a message"), default = 2)
     
-    def create_notice_types(app, created_models, verbosity, **kwargs):
-        NoticeType.create("user_message", _("Message you"), _("Someone send you a message"), default = 2)
-    
-    signals.post_syncdb.connect(create_notice_types, sender=NoticeType)
-
-else:
-    print "Skipping creation of NoticeTypes as notification app not found"
+signals.post_syncdb.connect(create_notice_types, sender=site_app)
